33665acdd11a373407bfb49dfa405de16bd7dc74
Cristian Santos <csantos@nativapps.co>
2019-02-28 23:07:42 +0000

    Ajustes sobre la JSON de respuesta

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -2,22 +2,29 @@ const { serverResponse } = require('../lib/responses');
 const Services = require('../lib/request');
 
 exports.getStockInformation = async (req, res) => {
-    const [logo, news, price] = await Promise.all([
-        await Services.getCompanyLogo(req.params.symbol),
-        await Services.getCompanyNews(req.params.symbol),
-        await Services.getCompanyPrice(req.params.symbol)
-    ])
-
-    console.log(logo)
-    serverResponse(req, res, {
-        statusCode: 200,
-        message: 'Successful request',
-        data: {
-            logo: logo ? logo.url : null,
-            last_news: news ? news[0] : {},
-            last_stock_price: price ? price.latestPrice : null
-        }
-    })
+    try {
+        const [logo, news, price] = await Promise.all([
+            await Services.getCompanyLogo(req.params.symbol),
+            await Services.getCompanyNews(req.params.symbol),
+            await Services.getCompanyPrice(req.params.symbol)
+        ])
+    
+        serverResponse(req, res, {
+            statusCode: 200,
+            message: 'Successful request',
+            data: {
+                logo: logo ? logo.url : null,
+                news_url: news ? news[0].url : null,
+                last_stock_price: price ? price.latestPrice : null
+            }
+        })
+    } catch (error) {
+        serverResponse(req, res, {
+            statusCode: 500,
+            message: `Error: ${error.message} | ${error.stack}`,
+            data: {}
+        })
+    }
 }
 
 exports.errorRouteHandler = (req, res) => {
